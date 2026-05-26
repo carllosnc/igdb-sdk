28ed7e89add2c3420fe45019b6642a86a78ccd59
Carlos Costa <carllos.nc@gmail.com>
2026-05-26 18:24:39 -0300

    feat: add barrel exports for all modules

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+export { IGDBClient } from "./IGDBClient.js";
+export type { IGDBQuery, IGDBClientOptions } from "./IGDBClient.js";
+
+export { GameClient } from "./client/game/index.js";
+export { PlatformClient } from "./client/platform/index.js";
+export { CompanyClient } from "./client/company/index.js";
+export { AgeRatingClient } from "./client/age-rating/index.js";
+export { ArtworkClient } from "./client/artwork/index.js";
+export { CharacterClient } from "./client/character/index.js";
+export { CollectionClient } from "./client/collection/index.js";
+export { EventClient } from "./client/event/index.js";
+export { ExternalGameClient } from "./client/external-game/index.js";
+export { PopularityClient } from "./client/popularity/index.js";
+export { ReleaseDateClient } from "./client/release-date/index.js";
+export { ReportClient } from "./client/report/index.js";
+export { SearchClient } from "./client/search/index.js";
+export { WebsiteClient } from "./client/website/index.js";
+export { MiscClient } from "./client/misc/index.js";
+
+export * from "./types/index.js";
